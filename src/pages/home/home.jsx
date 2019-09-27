70e7701f77f7adb7c8165d20e7aa544bf6f8dc9f
Veflex <alexandrelesimple75@gmail.com>
2019-09-27 18:44:45 +0200

    creating 3 state instead of 1 to handle the different todos and their status

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,36 +1,57 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./home.scss";
 
 export default () => {
-  const [tasks, setTasks] = useState([]);
+  const [todos, setTodos] = useState([]);
+  const [inProgress, setInProgress] = useState([]);
+  const [dones, setDones] = useState([]);
   const [task, setTask] = useState("");
-  const [currentTask, setCurrentTask] = useState("");
   const [isInputShowing, setisInputShowing] = useState(false);
-  const addTask = () => {
-    setTasks(prevTasks => {
-      return [...prevTasks, { task, status: "todo" }];
-    });
+  const addNewTask = () => {
+    console.log(task);
+    addTask({ task, status: "todo" }, setTodos);
     setTask("");
   };
   const showAddInput = () => {
     return setisInputShowing(true);
   };
 
-  const putTaskInProgress = task => {
-    task.status = "progress";
-    setCurrentTask(task);
-    return;
-  };
-  const putTaskInDone = task => {
-    console.log("done", task);
-    task.status = "done";
-    setCurrentTask("");
-    return;
+  const changeTaskStatus = (task, newStatus) => {
+    removeTaskFromCurrentState(task);
+    addTaskToNewState(task, newStatus);
   };
 
-  useEffect(() => {
-    console.log("useeffet");
-  }, [tasks]);
+  const removeTaskFromCurrentState = task => {
+    const status = task.status;
+    if (status === "todo") removeTask(task, todos, setTodos);
+    else if (status === "inProgress")
+      removeTask(task, inProgress, setInProgress);
+    else if (status === "done") removeTask(task, dones, setDones);
+    else return "mauvais status";
+  };
+
+  const removeTask = (task, arr, setState) => {
+    const index = arr.indexOf(task);
+    const newArr = [...arr];
+    newArr.splice(index, 1);
+    setState(() => {
+      return [...newArr];
+    });
+  };
+
+  const addTaskToNewState = (task, newStatus) => {
+    task.status = newStatus;
+    if (newStatus === "todo") addTask(task, setTodos);
+    else if (newStatus === "inProgress") addTask(task, setInProgress);
+    else if (newStatus === "done") addTask(task, setDones);
+    else return "mauvais newStatus";
+  };
+
+  const addTask = (task, setState) => {
+    setState(prevTasks => {
+      return [...prevTasks, task];
+    });
+  };
 
   return (
     <>
@@ -49,7 +70,7 @@ export default () => {
                   setTask(e.target.value);
                 }}
               />
-              <button onClick={addTask}>Add Task</button>
+              <button onClick={addNewTask}>Add Task</button>
             </div>
           ) : (
             <div className="add add-first-step" onClick={showAddInput}>
@@ -57,26 +78,24 @@ export default () => {
               <span>Add a task</span>
             </div>
           )}
-          {tasks ? (
-            tasks.map((elem, i) => {
-              if (elem.status !== "todo") return "";
-
+          {todos ? (
+            todos.map((task, i) => {
               return (
                 <div key={"todo" + i} className="task">
                   <div className="p-container">
-                    <p>{elem.task}</p>
+                    <p>{task.task}</p>
                   </div>
                   <div className="buttons">
                     <button
                       onClick={() => {
-                        putTaskInProgress(elem);
+                        changeTaskStatus(task, "inProgress");
                       }}
                     >
                       In Progress
                     </button>
                     <button
                       onClick={() => {
-                        putTaskInDone(elem);
+                        changeTaskStatus(task, "done");
                       }}
                     >
                       Done
@@ -95,27 +114,24 @@ export default () => {
           <h2>In progress</h2>
         </header>
         <div className="body">
-          {tasks ? (
-            tasks.map((elem, i) => {
-              if (elem.status === "progress") {
-                return (
-                  <div key={"progress" + i} className="task">
-                    <div className="p-container">
-                      <p>{elem.task}</p>
-                    </div>
-                    <div className="buttons">
-                      <button
-                        onClick={() => {
-                          putTaskInDone(elem);
-                        }}
-                      >
-                        Done
-                      </button>
-                    </div>
+          {inProgress ? (
+            inProgress.map((task, i) => {
+              return (
+                <div key={"progress" + i} className="task">
+                  <div className="p-container">
+                    <p>{task.task}</p>
                   </div>
-                );
-              }
-              return "";
+                  <div className="buttons">
+                    <button
+                      onClick={() => {
+                        changeTaskStatus(task, "done");
+                      }}
+                    >
+                      Done
+                    </button>
+                  </div>
+                </div>
+              );
             })
           ) : (
             <></>
@@ -127,18 +143,15 @@ export default () => {
           <h2>Done</h2>
         </header>
         <div className="body">
-          {tasks ? (
-            tasks.map((elem, i) => {
-              if (elem.status === "done") {
-                return (
-                  <div key={"done" + i} className="task">
-                    <div className="p-container">
-                      <p>{elem.task}</p>
-                    </div>
+          {dones ? (
+            dones.map((task, i) => {
+              return (
+                <div key={"done" + i} className="task">
+                  <div className="p-container">
+                    <p>{task.task}</p>
                   </div>
-                );
-              }
-              return "";
+                </div>
+              );
             })
           ) : (
             <></>
